d36293412aa40feae461a140de80de7bebbc276b
Louis Johnson <louis.johnson@biarri.com>
2019-10-11 13:13:12 +1000

    added opengl

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 extern crate sdl2;
+extern crate gl;
 
 fn main() {
-    println!("Hello, world!");
     let sdl = sdl2::init().unwrap();
     let video_subsystem = sdl.video().unwrap();
     let window = video_subsystem
-        .window("Game", 900, 700)
+        .window("rg2d", 900, 700)
+        .opengl()
         .resizable()
         .build()
         .unwrap();
-
+    let gl_context = window.gl_create_context().unwrap();
     let mut event_pump = sdl.event_pump().unwrap();
     'main: loop {
         for event in event_pump.poll_iter() {
