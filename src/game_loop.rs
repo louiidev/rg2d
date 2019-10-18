daa38c34c3f41b9e228a02d941ccb9f450768ba2
Louis Johnson <louis.johnson@biarri.com>
2019-10-18 13:15:21 +1000

    added key listener to the game loop

--- src/game_loop.rs
+++ src/game_loop.rs
@@ -1,5 +1,8 @@
 use sdl2::image::{self, InitFlag};
 use sdl2::render::WindowCanvas;
+use std::collections::HashSet;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
 use sdl2::EventPump;
 use crate::context::Context;
 use std::time::Duration;
@@ -18,16 +21,63 @@ pub trait EventHandler {
     fn render(&mut self, _ctx: &mut Context) -> Result<(), String>;
 }
 
+pub fn main() -> Result<(), String> {
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+
+    let _window = video_subsystem.window("Keyboard", 800, 600)
+        .position_centered()
+        .build()
+        .map_err(|e| e.to_string())?;
+
+    let mut events = sdl_context.event_pump().unwrap();
+
+    let mut prev_keys = HashSet::new();
+
+    'running: loop {
+        for event in events.poll_iter() {
+            if let Event::Quit {..} = event {
+                break 'running;
+            };
+        }
+
+        // Create a set of pressed Keys.
+        let keys = events.keyboard_state().pressed_scancodes().filter_map(Keycode::from_scancode).collect();
+
+        // Get the difference between the new and old sets.
+        let new_keys = &keys - &prev_keys;
+        let old_keys = &prev_keys - &keys;
+
+        if !new_keys.is_empty() || !old_keys.is_empty() {
+            println!("new_keys: {:?}\told_keys:{:?}", new_keys, old_keys);
+        }
+
+        prev_keys = keys;
+
+        std::thread::sleep(Duration::from_millis(100));
+    }
+
+    Ok(())
+}
+
 
 impl GameLoop {
-  pub fn run<S>(mut ctx: &mut Context, event_pump: &mut EventPump, state: &mut S) -> Result<(), String> where S: EventHandler {
+  pub fn run<S>(mut ctx: &mut Context, events: &mut EventPump, state: &mut S) -> Result<(), String> where S: EventHandler {
+    let mut prev_keys = HashSet::new();
     'main: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                sdl2::event::Event::Quit { .. } => break 'main,
-                _ => {}
-            }
+        for event in events.poll_iter() {
+            if let Event::Quit {..} = event {
+                break 'main;
+            };
         }
+        let keys = events.keyboard_state().pressed_scancodes().filter_map(Keycode::from_scancode).collect();
+        let new_keys = &keys - &prev_keys;
+        let old_keys = &prev_keys - &keys;
+
+        if !new_keys.is_empty() || !old_keys.is_empty() {
+            println!("new_keys: {:?}\told_keys:{:?}", new_keys, old_keys);
+        }
+        prev_keys = keys;
         state.update(&mut ctx);
         state.render(&mut ctx);
         std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
